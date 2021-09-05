1f7280a3cfaad8befb4b68ea3186578217c56940
Natalia2806 <tuirannatalia@gmail.com>
2021-09-04 23:35:30 -0500

    Implementando nest generator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UserTypesModule } from './user-types/user-types.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, UserTypesModule],
   controllers: [AppController],
   providers: [AppService],
 })
