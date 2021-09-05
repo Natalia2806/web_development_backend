fc499dda3cb7027073c2fb178c324bab441c7f42
Natalia2806 <tuirannatalia@gmail.com>
2021-09-04 23:14:03 -0500

    Metodos de peticion

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import path from 'path';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
+  @Get(':id/code/:code_user')
+  getHello(@Param ('id') id:number, @Param ('code_user') code: string ): string {
+    console.log('El parametro Id es', id);
+    console.log('El parametro code es', code);
     return this.appService.getHello();
+  }
+
+  @Post(':id')
+  postData(@Param('id') id: number, @Body()data:any): string {
+    console.log("Por el body se envia data",data);
+    return 'Buenas tardes';
   }
 }
